57003c8dc38580574b61d4e140cef728672c8e5e
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-07-01 18:00:45 -0400

    clear interval and rest intervals are working

--- src/components/workout-container/workout-details.tsx
+++ src/components/workout-container/workout-details.tsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import FormInput from '../ui-components/form-input';
+import WorkoutTimer from './workout-timer';
 import './workout-details.css'
 
-interface IFormState {
+export interface IFormState {
     numberOfSets: string;
     roundDuration: string;
     restDuration: string;
@@ -17,6 +18,7 @@ const WorkoutDetails: React.FC = () => {
         endOfRoundWarning: undefined,
     };
     const [form, setForm] = useState<IFormState>(initialState);
+    const [isStarted, setIsStarted] = useState<boolean>(false);
 
     const handleOnChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const name = e.target.name ?? e.currentTarget.name
@@ -24,19 +26,25 @@ const WorkoutDetails: React.FC = () => {
         setForm({...form, [name]: value});
     };
 
-    const handleOnSubmit = (e: React.FormEvent) => e.preventDefault();
+    const handleOnSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsStarted(true);
+    }
 
     const {numberOfSets, roundDuration, restDuration, endOfRoundWarning} = form;
     
     return (
-        <form className="workoutDetailsContainer" onSubmit={handleOnSubmit}>
-            <h1>Workout Interval Details</h1>
-            <FormInput name="numberOfSets" label="Number of Sets" value={numberOfSets} onChange={handleOnChange} />
-            <FormInput name="roundDuration" label="Round Duration (in seconds)" value={roundDuration} onChange={handleOnChange} />
-            <FormInput name="restDuration" label="Rest Duration (in seconds)" min="0" value={restDuration} onChange={handleOnChange} />
-            <FormInput name="endOfRoundWarning" label="End of Round Warning (in seconds)" type="select" options={["", "10","30"]} value={endOfRoundWarning} onChange={handleOnChange} />
-            <input type="submit" value="Start Workout" />
-        </form>
+        <>
+            <form className="workoutDetailsContainer" onSubmit={handleOnSubmit}>
+                <h1>Workout Interval Details</h1>
+                <FormInput name="numberOfSets" label="Number of Sets" value={numberOfSets} onChange={handleOnChange} />
+                <FormInput name="roundDuration" label="Round Duration (in seconds)" value={roundDuration} onChange={handleOnChange} />
+                <FormInput name="restDuration" label="Rest Duration (in seconds)" min="0" value={restDuration} onChange={handleOnChange} />
+                <FormInput name="endOfRoundWarning" label="End of Round Warning (in seconds)" type="select" options={["", "10","30"]} value={endOfRoundWarning} onChange={handleOnChange} />
+                <input type="submit" value="Start Workout" />
+            </form>
+            {isStarted && <WorkoutTimer numberOfSets={numberOfSets} roundDuration={roundDuration} restDuration={restDuration} endOfRoundWarning={endOfRoundWarning} />}
+        </>
     )
 }
 
