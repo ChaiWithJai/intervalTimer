ea8392ec630d520ec2b5a0bd72cbcc8821ab7e75
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-07-01 18:02:20 -0400

    timer

---
+++ src/components/workout-container/workout-timer.tsx
@@ -0,0 +1,47 @@
+import {addSeconds, differenceInSeconds} from 'date-fns';
+import React, {useState, useEffect} from 'react';
+import {IFormState as IWorkoutDetailsProps} from './workout-details';
+
+interface IInterval {
+    count: number,
+    isRest: boolean
+}
+
+interface ITimer {
+    timeLeft: number;
+    timeOver: Date;
+}
+
+const WorkoutTimer: React.FC<IWorkoutDetailsProps> = ({numberOfSets, roundDuration, restDuration, endOfRoundWarning}) => {
+    const [{count, isRest}, setCurrentInterval] = useState<IInterval>({count: 1, isRest: false});
+    const [{timeLeft, timeOver}, setTimer] = useState<ITimer>({timeLeft: Number(roundDuration), timeOver: addSeconds(new Date(), Number(roundDuration))});
+
+    const nextInterval = () => {
+        setCurrentInterval({isRest: false, count: count+1});
+        setTimer({timeLeft: Number(roundDuration), timeOver: addSeconds(new Date(), Number(roundDuration))})
+    }
+
+    const restInterval = () => {
+        setTimer({timeLeft: Number(restDuration), timeOver: addSeconds(new Date(), Number(restDuration))});
+        setCurrentInterval({count, isRest: true});
+    }
+    useEffect(() => {
+        const interval = setInterval(() => setTimer({timeOver, timeLeft: differenceInSeconds(timeOver, new Date())}), 500);
+        if (timeLeft <= 0) {
+            isRest ? nextInterval() : restInterval();
+            clearInterval(interval);
+            return;
+        }
+        return () => clearInterval(interval);
+    }, [timeLeft, isRest, timeOver]);
+    
+    return (
+        <>
+            <h1>{timeLeft}</h1>
+            <p>Current Round:  {count}</p>
+            <p>{isRest ? 'Rest up!' : `Let's get it!`}</p>
+        </>
+    );
+};
+
+export default WorkoutTimer;
